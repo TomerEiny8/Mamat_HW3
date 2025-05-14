d1f94f82dbc3eb5d02fc7750e28345066f959d47
TomerEiny8 <tomerkorial@gmail.com>
2025-05-14 11:49:20 +0300

    stack files

---
+++ stack.c
@@ -0,0 +1,80 @@
+#include "stack.h"
+#include <stdlib>
+
+struct stack {
+	elem_t* data;
+	size_t top;
+	size_t maxsize;
+	clone_t clone;
+	destroy_t destroy;
+	print_t print;
+};
+
+Stack stack_create(size_t max_size, clone_t clone, destroy_t destroy print_t print){
+	Stack s = malloc(sizeof(*s));
+	if(!s)
+		return NULL;
+	s->data = malloc(sizeof(elem_n) * max_size);
+	if(!s->data) {
+	        free(s);
+        	return NULL;
+	}
+ 	s->top = 0;
+	s->max_size = max_size;
+	s->clone = clone;
+	s->destroy = destroy;
+	s->print = print;
+	return s;
+}
+
+void stack_destroy(Stack s) {
+	if(!s) return;
+	for(size_t i = 0; i < s->top; ++i) {
+		s->destroy(s->data[i]);
+	}
+	free(s->data);
+	free(s);
+}
+
+bool stack_push(Stack s, elem_t elem) {
+	if(!s || s->top == s->max_size) 
+		return false;
+	elem_t copy = s->clone(elem);
+	if(!copy) 
+		return false;
+	s->data[s->top] = copy;
+	return true;
+}
+
+void stack_pop(Stack s) {
+	if(!s || s->top == 0)
+		return;
+	return s->data[--s->top];
+}
+
+elem_t stack_peek(Stack s) {
+	if(!s || s->top == 0)
+                return NULL;
+        return s->data[s->top - 1];
+}
+
+size_t stack_size(Stack s) {
+	return s ? s->top : 0;
+}
+
+bool stack_is_empty(Stack s) {
+	return s ? s->top ==0 : false;
+}
+
+size_t stack_capacity(Stack s) {
+	return s ? s->max_size - s->top : 0;
+}
+
+void stack_print(Stack s) {
+	if(!s)
+		return;
+	for(size_t i = s->top; i >= 0; --i) {
+		s->print(s->data[i]);
+	}
+}
+
---
+++ stack.h
@@ -0,0 +1,25 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef void* elem_t;
+
+typedef elem_t (*clone_y)(elem_t e);
+typedef void (*destroy_t)(elem_t e);
+typedef void (*print_t)(elem_t e);
+
+typedef struct stack* Stack;
+
+Stack stackcreate(size_t max_size, clone_t clone, destroy_t destroy, print_t print);
+void stack_destroy(Stack s);
+bool stack_push(Stack s, elem_t e);
+void stack_pop(Stack s);
+elem_t stack_peek(Stack s);
+size_t stack_size(Stack s);
+bool stack_is_empty(Stack s);
+size_t stack_capacity(Stack s);
+void stack_print(Stack s);
+
+#endif
