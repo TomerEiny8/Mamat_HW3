889274ad23f62f3f991b108d7b2ea10230271757
TomerEiny8 <tomerkorial@gmail.com>
2025-05-15 17:06:17 +0300

    Update student.c

--- student.c
+++ student.c
@@ -5,19 +5,22 @@
 
 void* student_clone(void* e) {
 	if(!e)
-		return NULL;
-	student* src = (student*)e;
-	student* clone = malloc(sizeof(student));
-	if(!clone)
-		return NULL;
-	clone->name = strdup(src->name);
-	if(!clone->name) {
-		free(clone);
-		return NULL;
-	}
-	clone->age = src->age;
-	clone->id = src->id;
-	return clone;
+        return NULL;
+    student* src = (student*)e;
+    student* clone = malloc(sizeof(student));
+    if(!clone)
+        return NULL;
+
+    size_t len = strlen(src->name) + 1;
+    clone->name = malloc(len);
+    if (!clone->name) {
+        free(clone);
+        return NULL;
+    }
+    memcpy(clone->name, src->name, len);
+    clone->age = src->age;
+    clone->id = src->id;
+    return clone;
 }
 
 void student_destroy(void* e) {
